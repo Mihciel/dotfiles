7640f8c46fb6a76eb9d3209215a886fc2b692dc2
Mihciel <verbauwhede.michiel@hotmail.com>
2021-07-31 01:38:44 +0200

    theming for alacritty and zsh

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
+sh ../colourscheme/genalacritty > alacritty.yml
+cat alacritty_end >> alacritty.yml
 [ -f $HOME/.config/alacritty.yml ] && echo "$HOME/.config/alacritty.yml is already present, please make a backup and remove before install." && exit 1 || ln -s $(pwd)/alacritty.yml $HOME/.config/alacritty.yml
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
+cat ../colourscheme/colours.sh > .zshrc
+cat .zshrc_end >> .zshrc
 [ -f $HOME/.zshrc ] && echo "$HOME/.zshrc is already present, please make a backup and remove before install." && exit 1 || ln -s $(pwd)/.zshrc $HOME/.zshrc
