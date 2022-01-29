bdeb5343875b5ba68f81bb13437055dc2de9128a
Mihciel <verbauwhede.michiel@hotmail.com>
2022-01-29 17:17:52 +0100

    generic update

--- pywal/new_theme.sh
+++ pywal/new_theme.sh
@@ -1,1 +1,1 @@
-wal -n -s -t -e -a 0.6 -i ~/.local/share/backgrounds/$1 --backend colorz -o "./post_new_theme.sh"
+wal -n -s -t -e -a 0.6 -i ` feh -. -B black -A "echo %F; kill %V" -r ~/.local/share/backgrounds` --backend colorz -o "./post_new_theme.sh"
